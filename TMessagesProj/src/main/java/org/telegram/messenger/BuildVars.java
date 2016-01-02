88bf66a3472dba328f3661a106407ffafd9cd16a
Navid <navid.ei2008@gmail.com>
2016-01-02 13:53:56 +0330

    BuildVars and build.gradle updated.

--- TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -20,15 +20,15 @@ public class BuildVars {
     public static String FOURSQUARE_API_KEY = ""; //obtain your own KEY at https://developer.foursquare.com/
     public static String FOURSQUARE_API_ID = ""; //obtain your own API_ID at https://developer.foursquare.com/
     public static String FOURSQUARE_API_VERSION = "20150326";*/
-    public static int APP_ID = 35353;
+    public static int APP_ID = 47329;
     //public static int APP_ID = 2458;
-    public static String APP_HASH = "d68d79b2ee161e4a39df6b1b11c93e1e";
+    public static String APP_HASH = "e26828b3aef4dc6fcefa56c087e06d81";
     //public static String APP_HASH = "5bce48dc7d331e62c955669eb7233217";
     public static String HOCKEY_APP_HASH = "aa53be0c2bc192b3da70f62f8c37df19";
     public static String HOCKEY_APP_HASH_DEBUG = "aa53be0c2bc192b3da70f62f8c37df19";
     //public static String GCM_SENDER_ID = "760348033672";
     public static String GCM_SENDER_ID = "407894285626";
-    public static String SEND_LOGS_EMAIL = "violetenet+telegram@gmail.com";
+    public static String SEND_LOGS_EMAIL = "violete.net@gmail.com";
     public static String BING_SEARCH_KEY = "Lc1qVtHS3sP47gnXcE6pVbihRRWEx1UoarUvQwgAAW4="; //obtain your own KEY at https://www.bing.com/dev/en-us/dev-center
     public static String FOURSQUARE_API_KEY = "XUZJYAFMZR1KSRVLWXLZ0OU3EJ3MXLOA30KZKUJHVHWRA3SQ"; //obtain your own KEY at https://developer.foursquare.com/
     public static String FOURSQUARE_API_ID = "TWHORZ3Y4CXA1UCFURW23AI3IWHG22FXK43Q4NR3LNDYPML5"; //obtain your own API_ID at https://developer.foursquare.com/
